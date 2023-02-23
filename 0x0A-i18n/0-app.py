b8edbd1cb0275bff48cdbc2d2fe972de53dc03b1
Ghassenhedfii <3681@holbertonstudents.com>
2023-02-23 22:49:01 +0100

    basic flask app

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
-
+"""
+Route module for the API
+"""
 from flask import Flask, render_template
-app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"], strict_slashes=False)
-def hello_world():
-    ''' return the template '''
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/')
+def index():
+    """basic index"""
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
