c55c21c948e9abac91579aae4a5f24385f5ac0d5
ghassenhedfii <3681@holbertonstudents.com>
2023-05-30 14:15:41 +0200

    nosql

---
+++ 0x0D-NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+a Python function that inserts a new document 
+in a collection based on kwargs
+"""
+
+def insert_school(mongo_collection, **kwargs):
+    """ inserts a new document in a collection """
+    _id = mongo_collection.insert(kwargs)
+    if mongo_collection:
+        return _id
