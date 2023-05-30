f8076f2e0a7d680995e304c16afe47eeb8507967
ghassenhedfii <3681@holbertonstudents.com>
2023-05-30 14:58:22 +0200

    njsBasic

---
+++ 0x12-Node_JS_basic/2-main_1.js
@@ -0,0 +1,6 @@
+const countStudents = require('./2-read_file');
+
+countStudents("database.csv");
+const countStudents = require('./2-read_file');
+
+countStudents("database.csv");
---
+++ 0x12-Node_JS_basic/5-http.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hello Holberton School!');
+    res.end();
+  }
+  if (req.url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('This is the list of our students');
+    res.end();
+  }
+});
+
+app.listen(port, hostname, () => {
+});
+
+module.exports = app;
